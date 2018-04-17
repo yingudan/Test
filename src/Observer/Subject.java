e61c2471486f3eab8737373b724fc52ac2a2ee2c
yingudan <yingudan@vip.qq.com>
2018-04-17 15:59:07 +0800

    sss

--- src/Observer/Subject.java
+++ src/Observer/Subject.java
@@ -4,7 +4,12 @@ package Observer;
  * 建立目标
  */
 public interface Subject {
-	public void attach(Observer o);//观察操作
-    public void detach(Observer o);
-    public void notice();
+	public void attach(Observer o);// 观察操作
+
+	public void detach(Observer o);
+
+	public void notice();
+	
+	
+
 }
---
+++ src/demo/Gun.java
@@ -0,0 +1,15 @@
+package demo;
+/** 
+* @author shandowF 
+* @date  2018年3月15日 
+*/
+public class Gun {
+
+	
+	public static void main(String[] args) {
+		System.out.println(System.getenv());
+	}
+	
+	
+	
+}
